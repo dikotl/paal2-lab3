d2eef4c70c17266b54ba81d61bee08371dc97212
Remenod <remenod@gmail.com>
2025-03-13 19:52:24 +0200

    Generic test system

--- ClassLibraryTests/TestFeo.cs
+++ ClassLibraryTests/TestFeo.cs
@@ -2,12 +2,17 @@
 using FunctionalEnumerableOperations;
 using Feo = FunctionalEnumerableOperations;
 using static ClassLibraryTests.GlobalRandom;
+using System.Numerics;
 
 namespace ClassLibraryTests;
 
 [TestClass]
 public sealed class TestFeo
 {
+    const int repeatTime = 300;
+    const int defMaxSize = 600;
+    const int defElementBound = 10000;
+
 
     private int[] GetIntRandArray(int maxSize, uint maxElementBound)
     {
@@ -32,50 +37,62 @@ public sealed class TestFeo
 
 
     [TestMethod]
-    public void TestMinMaxInt()
-    {
-        var a = GetIntRandArray(600, 10000);
-        var expectedMin = System.Linq.Enumerable.Min(a);
-        var expectedMax = System.Linq.Enumerable.Max(a);
-        var min = a.Min();
-        var max = a.Max();
-
-        Assert.AreEqual(min, expectedMin, "Feo.Min <int> does not work correctly");
-        Assert.AreEqual(max, expectedMax, "Feo.Max <int> does not work correctly");
-    }
-    [TestMethod]
-    public void TestMinMaxDouble()
-    {
-        var a = GetDoubleRandArray(600, 10000);
-        var expectedMin = System.Linq.Enumerable.Min(a);
-        var expectedMax = System.Linq.Enumerable.Max(a);
-        var min = a.Min();
-        var max = a.Max();
-
-        Assert.AreEqual(min, expectedMin, "Feo.Min <double> does not work correctly");
-        Assert.AreEqual(max, expectedMax, "Feo.Max <double> does not work correctly");
-    }
+    public void TestMinMax___Int() => TestMinMax(GetIntRandArray);
 
     [TestMethod]
-    public void TestSkipInt()
-    {
-        var a = GetIntRandArray(600, 10000);
-        var skipLen = Rand.Next(a.Length);
-        var expected = String.Join(" ", System.Linq.Enumerable.Skip(a, skipLen));
-        var actual = String.Join(" ", a.Skip(skipLen));
-
-        Assert.AreEqual(actual, expected, "Feo.Skip <int> does not work correctly");
-    }
+    public void TestMinMax___Double() => TestMinMax(GetDoubleRandArray);
 
     [TestMethod]
-    public void TestSkipDouble()
-    {
-        var a = GetIntRandArray(600, 10000);
-        var skipLen = Rand.Next(a.Length);
-        var expected = String.Join(" ", System.Linq.Enumerable.Skip(a, skipLen));
-        var actual = String.Join(" ", a.Skip(skipLen));
+    public void TestSkip___Int() => TestSkip(GetIntRandArray);
 
-        Assert.AreEqual(actual, expected, "Feo.Skip <double> does not work correctly");
+    [TestMethod]
+    public void TestSkip___Double() => TestSkip(GetDoubleRandArray);
+
+    [TestMethod]
+    public void TestTake___Int() => TestTake(GetIntRandArray);
+
+    [TestMethod]
+    public void TestTake___Double() => TestTake(GetDoubleRandArray);
+
+    private void TestMinMax<T>(Func<int, uint, T[]> generateArray) where T : IComparisonOperators<T, T, bool>
+    {
+        for (var i = 0; i < repeatTime; i++)
+        {
+            var a = generateArray(defMaxSize, defElementBound);
+            var expectedMin = a.Min();
+            var expectedMax = a.Max();
+            var min = System.Linq.Enumerable.Min(a);
+            var max = System.Linq.Enumerable.Max(a);
+
+            Assert.AreEqual(min, expectedMin, $"Feo.Min <{typeof(T)}> does not work correctly");
+            Assert.AreEqual(max, expectedMax, $"Feo.Max <{typeof(T)}> does not work correctly");
+        }
+    }
+
+    private void TestSkip<T>(Func<int, uint, T[]> generateArray)
+    {
+        for (var i = 0; i < repeatTime; i++)
+        {
+            var a = generateArray(defMaxSize, defElementBound);
+            var skipLen = Rand.Next(a.Length);
+            var expected = string.Join(" ", System.Linq.Enumerable.Skip(a, skipLen));
+            var actual = string.Join(" ", a.Skip(skipLen));
+
+            Assert.AreEqual(actual, expected, $"Feo.Skip <{typeof(T)}> does not work correctly");
+        }
+    }
+
+    private void TestTake<T>(Func<int, uint, T[]> generateArray)
+    {
+        for (var i = 0; i < repeatTime; i++)
+        {
+            var a = generateArray(defMaxSize, defElementBound);
+            var takeLen = Rand.Next(a.Length);
+            var expected = string.Join(" ", System.Linq.Enumerable.Take(a, takeLen));
+            var actual = string.Join(" ", a.Take(takeLen));
+
+            Assert.AreEqual(actual, expected, $"Feo.Take <{typeof(T)}> does not work correctly");
+        }
     }
 }
 
