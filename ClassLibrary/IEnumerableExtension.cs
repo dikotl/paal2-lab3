ea46dc70197390d0470f4eeed52e167cdd8b2029
Remenod <remenod@gmail.com>
2025-03-15 18:06:17 +0200

    move out sorters depentions to global scope

--- ClassLibrary/IEnumerableExtension.cs
+++ ClassLibrary/IEnumerableExtension.cs
@@ -204,7 +204,8 @@ public static class IEnumerableExtension
         return source.CreateOrderedEnumerable(keySelector, comparer ?? Comparer<TKey>.Default, true);
     }
 
-    internal abstract class OrderedEnumerable<TElement> : System.Linq.IOrderedEnumerable<TElement>
+}
+internal abstract class OrderedEnumerable<TElement> : System.Linq.IOrderedEnumerable<TElement>
     {
         internal IEnumerable<TElement> source;
 
@@ -235,109 +236,108 @@ public static class IEnumerableExtension
             return result;
         }
     }
-    internal class OrderedEnumerable<TElement, TKey> : OrderedEnumerable<TElement>
+internal class OrderedEnumerable<TElement, TKey> : OrderedEnumerable<TElement>
+{
+    internal OrderedEnumerable<TElement>? parent;
+    internal Func<TElement, TKey> keySelector;
+    internal IComparer<TKey> comparer;
+    internal bool descending;
+
+    internal OrderedEnumerable(IEnumerable<TElement> source, Func<TElement, TKey> keySelector, IComparer<TKey>? comparer, bool descending) : base(source)
     {
-        internal OrderedEnumerable<TElement>? parent;
-        internal Func<TElement, TKey> keySelector;
-        internal IComparer<TKey> comparer;
-        internal bool descending;
-
-        internal OrderedEnumerable(IEnumerable<TElement> source, Func<TElement, TKey> keySelector, IComparer<TKey>? comparer, bool descending) : base(source)
-        {
-            this.parent = null;
-            this.keySelector = keySelector;
-            this.comparer = comparer != null ? comparer : Comparer<TKey>.Default;
-            this.descending = descending;
-        }
-
-        internal override EnumerableSorter<TElement> GetEnumerableSorter(EnumerableSorter<TElement>? next)
-        {
-            EnumerableSorter<TElement> sorter = new EnumerableSorter<TElement, TKey>(keySelector, comparer, descending, next);
-            if (parent != null) sorter = parent.GetEnumerableSorter(sorter);
-            return sorter;
-        }
+        this.parent = null;
+        this.keySelector = keySelector;
+        this.comparer = comparer != null ? comparer : Comparer<TKey>.Default;
+        this.descending = descending;
     }
-    internal abstract class EnumerableSorter<TElement>
+
+    internal override EnumerableSorter<TElement> GetEnumerableSorter(EnumerableSorter<TElement>? next)
     {
-        internal abstract void ComputeKeys(TElement[] elements, int count);
+        EnumerableSorter<TElement> sorter = new EnumerableSorter<TElement, TKey>(keySelector, comparer, descending, next);
+        if (parent != null) sorter = parent.GetEnumerableSorter(sorter);
+        return sorter;
+    }
+}
+internal abstract class EnumerableSorter<TElement>
+{
+    internal abstract void ComputeKeys(TElement[] elements, int count);
 
-        internal abstract int CompareKeys(int index1, int index2);
+    internal abstract int CompareKeys(int index1, int index2);
 
-        internal int[] Sort(TElement[] elements, int count)
+    internal int[] Sort(TElement[] elements, int count)
+    {
+        ComputeKeys(elements, count);
+        int[] map = new int[count];
+        for (int i = 0; i < count; i++) map[i] = i;
+        QuickSort(map, 0, count - 1);
+        return map;
+    }
+
+    void QuickSort(int[] map, int left, int right)
+    {
+        do
         {
-            ComputeKeys(elements, count);
-            int[] map = new int[count];
-            for (int i = 0; i < count; i++) map[i] = i;
-            QuickSort(map, 0, count - 1);
-            return map;
-        }
-
-        void QuickSort(int[] map, int left, int right)
-        {
+            int i = left;
+            int j = right;
+            int x = map[i + ((j - i) >> 1)];
             do
             {
-                int i = left;
-                int j = right;
-                int x = map[i + ((j - i) >> 1)];
-                do
+                while (i < map.Length && CompareKeys(x, map[i]) > 0) i++;
+                while (j >= 0 && CompareKeys(x, map[j]) < 0) j--;
+                if (i > j) break;
+                if (i < j)
                 {
-                    while (i < map.Length && CompareKeys(x, map[i]) > 0) i++;
-                    while (j >= 0 && CompareKeys(x, map[j]) < 0) j--;
-                    if (i > j) break;
-                    if (i < j)
-                    {
-                        int temp = map[i];
-                        map[i] = map[j];
-                        map[j] = temp;
-                    }
-                    i++;
-                    j--;
-                } while (i <= j);
-                if (j - left <= right - i)
-                {
-                    if (left < j) QuickSort(map, left, j);
-                    left = i;
+                    int temp = map[i];
+                    map[i] = map[j];
+                    map[j] = temp;
                 }
-                else
-                {
-                    if (i < right) QuickSort(map, i, right);
-                    right = j;
-                }
-            } while (left < right);
-        }
-    }
-    internal class EnumerableSorter<TElement, TKey> : EnumerableSorter<TElement>
-    {
-        internal Func<TElement, TKey> keySelector;
-        internal IComparer<TKey> comparer;
-        internal bool descending;
-        internal EnumerableSorter<TElement>? next;
-        internal TKey[] keys = [];
-
-        internal EnumerableSorter(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending, EnumerableSorter<TElement>? next)
-        {
-            this.keySelector = keySelector;
-            this.comparer = comparer;
-            this.descending = descending;
-            this.next = next;
-        }
-
-        internal override void ComputeKeys(TElement[] elements, int count)
-        {
-            keys = new TKey[count];
-            for (int i = 0; i < count; i++) keys[i] = keySelector(elements[i]);
-            if (next != null) next.ComputeKeys(elements, count);
-        }
-
-        internal override int CompareKeys(int index1, int index2)
-        {
-            int c = comparer.Compare(keys[index1], keys[index2]);
-            if (c == 0)
+                i++;
+                j--;
+            } while (i <= j);
+            if (j - left <= right - i)
             {
-                if (next == null) return index1 - index2;
-                return next.CompareKeys(index1, index2);
+                if (left < j) QuickSort(map, left, j);
+                left = i;
             }
-            return descending ? -c : c;
+            else
+            {
+                if (i < right) QuickSort(map, i, right);
+                right = j;
+            }
+        } while (left < right);
+    }
+}
+internal class EnumerableSorter<TElement, TKey> : EnumerableSorter<TElement>
+{
+    internal Func<TElement, TKey> keySelector;
+    internal IComparer<TKey> comparer;
+    internal bool descending;
+    internal EnumerableSorter<TElement>? next;
+    internal TKey[] keys = [];
+
+    internal EnumerableSorter(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending, EnumerableSorter<TElement>? next)
+    {
+        this.keySelector = keySelector;
+        this.comparer = comparer;
+        this.descending = descending;
+        this.next = next;
+    }
+
+    internal override void ComputeKeys(TElement[] elements, int count)
+    {
+        keys = new TKey[count];
+        for (int i = 0; i < count; i++) keys[i] = keySelector(elements[i]);
+        if (next != null) next.ComputeKeys(elements, count);
+    }
+
+    internal override int CompareKeys(int index1, int index2)
+    {
+        int c = comparer.Compare(keys[index1], keys[index2]);
+        if (c == 0)
+        {
+            if (next == null) return index1 - index2;
+            return next.CompareKeys(index1, index2);
         }
+        return descending ? -c : c;
     }
 }
