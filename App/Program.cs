351657e5280e1ad631a8221cb9b4cc541ad96b70
dikotl <188217911+dikotl@users.noreply.github.com>
2025-03-15 17:17:25 +0200

    `DynArray<T>` implementation (#3)
    * Initial array implementation (WIP)
    
    * Fix `Array<T>.ToString()` elements not separated by a comma
    
    * Finish `Array<T>.Resize()`
    
    * Fix `Array<T>.Resize()` for 0 length
    
    * Implement `Add` and `Clear`
    
    * Implement `+` operator
    
    * Move `Count` property setter to `Resize` method
    
    * Decrease initial capacity
    
    * Implement more methods
    
    * Rename class to `DynArray`
    
    * Rename file
    
    * Fix nullability warnings, implement more methods
    
    * Backward indexing, slicing
    
    * Equals, GetHashCode
    
    * Implement `ICloneable`
    
    * Fix compile errors, make `App` a library

--- App/Program.cs
+++ App/Program.cs
@@ -3,13 +3,8 @@ using System.IO;
 
 namespace App;
 
-public static class App
+public static class Program
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello world!");
-    }
-
     public static void Task1(TextReader reader, TextWriter writer) => throw new NotImplementedException();
     public static void Task2(TextReader reader, TextWriter writer) => throw new NotImplementedException();
     public static void Task3(TextReader reader, TextWriter writer) => throw new NotImplementedException();
