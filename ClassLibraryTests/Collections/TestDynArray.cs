38d97ce31794fd6ac3681ab83726b4270042a68f
dikotl <188217911+dikotl@users.noreply.github.com>
2025-03-19 20:14:38 +0200

    `DynArray` tests placeholder

---
+++ ClassLibraryTests/Collections/TestDynArray.cs
@@ -0,0 +1,141 @@
+namespace ClassLibraryTests.Collections.DynArray;
+
+[TestClass]
+public sealed class Basics
+{
+    [TestMethod]
+    public void TestGetSetIndex()
+    {
+    }
+
+    [TestMethod]
+    public void TestGetCount()
+    {
+    }
+
+    [TestMethod]
+    public void TestGetCapacity()
+    {
+    }
+
+    [TestMethod]
+    public void TestGetEnumerator()
+    {
+    }
+
+    [TestMethod]
+    public void TestCtors()
+    {
+    }
+
+    [TestMethod]
+    public void TestSlice()
+    {
+    }
+}
+
+[TestClass]
+public sealed class ListOps
+{
+    [TestMethod]
+    public void TestIndexOf()
+    {
+    }
+
+    [TestMethod]
+    public void TestInsert()
+    {
+    }
+
+    [TestMethod]
+    public void TestRemove()
+    {
+    }
+
+    [TestMethod]
+    public void TestAdd()
+    {
+    }
+
+    [TestMethod]
+    public void TestClear()
+    {
+    }
+
+    [TestMethod]
+    public void TestContains()
+    {
+    }
+}
+
+[TestClass]
+public sealed class ResizeOps
+{
+    [TestMethod]
+    public void TestGrowFactor()
+    {
+    }
+
+    [TestMethod]
+    public void TestReserve()
+    {
+    }
+
+    [TestMethod]
+    public void TestResize()
+    {
+    }
+
+    [TestMethod]
+    public void TestAdd()
+    {
+    }
+
+    [TestMethod]
+    public void TestInsert()
+    {
+    }
+
+    [TestMethod]
+    public void TestRemove()
+    {
+    }
+}
+
+[TestClass]
+public sealed class Ops
+{
+    [TestMethod]
+    public void TestOpEquals()
+    {
+    }
+
+    [TestMethod]
+    public void TestOpPlus()
+    {
+    }
+}
+
+[TestClass]
+public sealed class Other
+{
+    [TestMethod]
+    public void TestCopyTo()
+    {
+    }
+
+    [TestMethod]
+    public void TestToString()
+    {
+    }
+
+    [TestMethod]
+    public void TestEquals()
+    {
+    }
+
+    [TestMethod]
+    public void TestClone()
+    {
+    }
+}
