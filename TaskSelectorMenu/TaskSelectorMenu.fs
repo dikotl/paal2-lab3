b7f272f1510125e4df57f45c4970765af0cdbf1f
dikotl <188217911+dikotl@users.noreply.github.com>
2025-04-01 23:38:24 +0300

    Support multiple input files through command line arguments (#34)
    * Fix `Context.Write`
    
    * Fix formatting
    
    * Implement multi-file input through command line arguments
    
    Now the output file is not used, so the bash `>` operator should be used instead
    
    * Fixes

--- TaskSelectorMenu/TaskSelectorMenu.fs
+++ TaskSelectorMenu/TaskSelectorMenu.fs
@@ -9,7 +9,7 @@ type Tasks = Dictionary<int, struct (Action<Context> * string)>
 type TaskList = Action<Context> List
 
 
-let taskMenu () =
+let generateMenuAndTasks () =
     let menu = new StringBuilder()
     let tasks = new TaskList()
     let taskIndex = ref 0
@@ -51,37 +51,57 @@ let rec selectTask (tasks: TaskList) (context: Context) =
         tasks[taskIndex]
 
 
+let rec runTaskSelector menu tasks (context: Context) =
+    context.PrintLine menu
+
+    try
+        let task = selectTask tasks context
+
+        context.PrintLine "To return to the menu, type 'menu'"
+        context.PrintLine "To exit the program, type 'exit'"
+
+        task.Invoke context
+    with
+    | :? ExitProgramException -> exit 0
+    | :? ExitToMenuException -> ()
+
+    if context.TalkToUser then
+        // We are not in "test mode", so run the menu again
+        runTaskSelector menu tasks context
+
+
+/// Opens a file and runs the action if it's exists.
+///
+/// A file may contains the following:
+///
+/// ```txt
+/// 1
+/// 1
+/// 10
+/// ```
+///
+/// ... where the first line contains the task number, and the rest - the sequence of input data.
+let openAndRun (filepath: string) applyReader =
+    try
+        use reader = new StreamReader(filepath) :> TextReader
+        applyReader reader
+    with :? IOException as e ->
+        eprintfn $"Error while processing program arguments. {e.Message}"
+
+
 [<EntryPoint>]
 let main args =
-    use reader =
-        if args.Length > 0 then
-            new StreamReader(args[0]) :> TextReader
-        else
-            Console.In
+    let menu, tasks = generateMenuAndTasks ()
+    let run = runTaskSelector menu tasks
 
-    use writer =
-        if args.Length > 1 then
-            new StreamWriter(args[1]) :> TextWriter
-        else
-            Console.Out
+    let runWithReader reader =
+        let context = Context(reader, Console.Out, (reader = Console.In))
+        run context
 
-    let context = Context(reader, writer, args.Length = 0)
-    let menu, tasks = taskMenu ()
+    if args.Length > 0 then
+        for filepath in args do
+            openAndRun filepath runWithReader
+    else
+        runWithReader Console.In
 
-    let rec runTaskSelector () =
-        try
-            context.PrintLine menu
-
-            let task = selectTask tasks context
-
-            context.PrintLine "To return to the menu, type 'menu'"
-            context.PrintLine "To exit the program, type 'exit'"
-            task.Invoke context
-
-            runTaskSelector ()
-        with
-        | :? ExitToMenuException -> runTaskSelector ()
-        | :? ExitProgramException -> ()
-
-    runTaskSelector ()
     0
